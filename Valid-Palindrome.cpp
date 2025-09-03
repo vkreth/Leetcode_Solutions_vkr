aba70d7489348506c4b8c81514432fd2e8e71615
Vamsi Krishna Akula <avkrao0101@gmail.com>
2025-09-03 15:22:27 -0500

    .

--- Valid-Palindrome.cpp
+++ Valid-Palindrome.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
+bool isaln(char c)
+{
+    if((c>='0' && c<='9')||(tolower(c)>='a' && tolower(c)<='z')){
+        return true;
+    }
+    return false;
+}
     bool isPalindrome(string s) {
-        string m;
-        for(char c:s)
-        {
-            if(isalnum(c))
-            {
-                m+=tolower(c);
+        int s1=0, s2=s.length()-1;
+        while(s1<s2){
+            if(!isaln(s[s1])){
+                s1++; continue;
             }
+            if(!isaln(s[s2])){
+                s2--; continue;
+            }
+            if(tolower(s[s1])!=tolower(s[s2])){
+                return false;
+            }
+            s1++;s2--;
         }
-        string x=m;
-        reverse(x.begin(),x.end());
-        return x==t;
+        return true;
     }
 };
