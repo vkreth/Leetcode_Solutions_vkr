6009ec618b0655a31cdc833fe3ddf2782dec300c
Vamsi Krishna Akula <avkrao0101@gmail.com>
2025-10-16 14:39:36 -0500

    Implement function to detect cycle in linked list

---
+++ 141. Linked-list-cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let nodespresent=new Set();
+    let current=head;
+    while(current!=null)
+    {
+        if(nodespresent.has(current))
+        {
+            return true;
+        }
+        nodespresent.add(current);
+        current=current.next;
+    }
+    return false;
+};
