ade951f29e65186d00084088031bbdf5e8579d3c
Vamsi Krishna Akula <avkrao0101@gmail.com>
2025-10-29 13:52:53 -0500

    Add oddEvenList function for linked list rearrangement
    Implement function to rearrange linked list by odd and even nodes.

---
+++ Odd-Even-Linked-List.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if(head==null || head.next==null)
+    {
+        return head;
+    }
+    let odd=head;
+    let even=odd.next;
+    let evenstart=even;
+    while(odd.next!=null && even.next!=null)
+    {
+        odd.next=odd.next.next;
+        even.next=even.next.next;
+        odd=odd.next;
+        even=even.next;
+    }
+    odd.next=evenstart;
+    return head;
+};
